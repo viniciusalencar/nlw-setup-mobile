287605ddff1478a1b58feb73cef8b40e375b3bb4
viniciusalencar <viniciusalencar176@gmail.com>
2023-01-24 22:00:55 -0300

    apply semi bold to checkbox title

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -27,7 +27,7 @@ export function Checkbox({ title, checked = false, ...rest}: Props) {
       <View className='h-8 w-8 bg-zinc-900 rounded-lg'/>
     )}
 
-    <Text className='text-white ml-3 text-base'>
+    <Text className='text-white ml-3 text-base font-semibold'>
       {title}
     </Text>
       
